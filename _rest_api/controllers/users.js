f055d2cc124e878913ad3c4e26571613195f20aa
Kevin-Jose-Sandoval <2819606230104@ingenieria.usac.edu.gt>
2021-09-01 20:04:49 -0600

    Finish rest api

---
+++ _rest_api/controllers/users.js
@@ -0,0 +1,58 @@
+const { v4 } = require('uuid')
+let data = []
+
+const getUsers = (req, res) => {
+    res.status(200).json(data)
+}
+
+const getUser = (req, res) => {
+    const { id } = req.params;
+
+    const foundUser = data.find((user) => 
+        user.id == id
+    )
+
+    if (foundUser == undefined){
+        res.status(404).json({error : "User id not found"});
+    }else {
+        res.status(200).json(foundUser)
+    }    
+};
+
+const createUser = (req, res) => {
+    const new_user = req.body;
+
+    const user_with_id = { ...new_user, id: v4() }
+    data.push(user_with_id)
+
+    res.status(201).json('User with the name ' + new_user.firstName + ' added to data')
+};
+
+const deleteUser =  (req, res) => {
+    const { id } = req.params;
+
+    data = data.filter((user) => user.id != id);
+
+    res.status(200).json('User with the id ' + id + ' deleted to data');
+};
+
+const updateUser = (req, res) => {
+    const { id } = req.params;
+    const { firstName, lastName, age } = req.body;
+    
+    const user = data.find((user) => user.id == id)
+
+    if(firstName) user.firstName = firstName;
+    if(lastName) user.lastName = lastName;
+    if(age) user.age = age;
+
+    res.status(201).json('User with the id ' + id + ' has been updated');
+};
+
+module.exports = {
+    getUsers,
+    getUser,
+    createUser,
+    deleteUser,
+    updateUser
+}
--- _rest_api/routes/users.js
+++ _rest_api/routes/users.js
@@ -1,36 +1,16 @@
 const { Router } = require('express');
+const { getUsers, createUser, getUser, deleteUser, updateUser } = require('../controllers/users');
 const router = new Router();
 
-const data = require('../data.json')
 
-router.get('/', (req, res) => {
-    res.status(200).json(data)
-});
+router.get('/users', getUsers);
 
-router.post('/users', (req, res) => {
-    const id = data.length + 1;
-    const new_user = {id, ...req.body};
+router.get('/users/:id', getUser);
 
-    data.push(new_user)
-    res.json(data)
-});
+router.post('/users', createUser);
 
-router.delete('/users/:id', (req, res) => {
-    const id = req.params.id;
-    let found_id = false;
+router.delete('/users/:id', deleteUser);
 
-    for (let i = 0; i < data.length; i++){
-        if (data[i].id == id){
-            data.splice(i, 1);
-            found_id = true;
-        }
-    }
-
-    if (!found_id){
-        res.status(404).json({error : "User id not found"});
-    }else {
-        res.json(data)
-    }
-});
+router.patch('/users/:id', updateUser);
 
 module.exports = router;
